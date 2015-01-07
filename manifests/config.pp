4d144e8b1839540889d547db8d9d7ff6eda19667
Martin Helmich <m.helmich@mittwald.de>
2015-01-07 16:42:35 +0100

    pear::config fails on empty values

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,6 @@ define pear::config ($property = $title, $value) {
   exec { "pear-config-${property}":
     command => "pear config-set ${property} \"${value}\"",
     path    => ["/bin","/usr/bin","/usr/local/bin"],
-    unless  => "[ $(pear config-get ${property}) = \"${value}\" ]"
+    unless  => "[ \"$(pear config-get ${property})\" = \"${value}\" ]"
   }
 }
